4f079a4f9ea63425427c9b50f6b59a296fbacb67
Joseph Fletcher <u1863522@unimail.hud.ac.uk>
2019-02-25 16:28:49 +0000

    Initial commit

--- src/controller/HelperClass.java
+++ src/controller/HelperClass.java
@@ -2,21 +2,8 @@ package controller;
 
 import org.apache.commons.validator.routines.EmailValidator;
 
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
 
 public class HelperClass {
-
-   /* public static boolean isEmailAddressValid(String email) {
-        boolean result = true;
-        try {
-            InternetAddress emailAddr = new InternetAddress(email);
-            emailAddr.validate();
-        } catch (AddressException ex) {
-            result = false;
-        }
-        return result;
-    }*/
 
     public static boolean isEmailAddressValid(String email) {
         EmailValidator validator = EmailValidator.getInstance();
